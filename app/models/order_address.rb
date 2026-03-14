01bf9e032714a28d3c1ad87407790f3d3ebcb11d
251017-web <zlba251017@gmail.com>
2026-03-15 04:37:43 +0900

    購入フォームのバリデーションと文言を見本準拠に調整

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,16 +2,18 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :block, :building, :phone_number, :token
 
-  with_options presence: true do
-    validates :user_id
-    validates :item_id
-    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :city
-    validates :block
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
-    validates :token
-  end
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+  validates :token, presence: true
+  validates :post_code, presence: true
+  validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :city, presence: true
+  validates :block, presence: true
+  validates :phone_number, presence: true
+  validates :phone_number, length: { minimum: 10, message: 'is too short' }
+  validates :phone_number, format: { with: /\A\d+\z/, message: 'is invalid. Input only number' }
+  validates :phone_number, length: { maximum: 11 }, allow_blank: true
 
   def save
     ActiveRecord::Base.transaction do
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -38,12 +38,13 @@ RSpec.describe OrderAddress, type: :model do
       it 'post_codeが空だと保存できないこと' do
         @order_address.post_code = ''
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Post code can't be blank")
+        expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+        expect(@order_address.errors.full_messages).to include('Postal code is invalid. Enter it as follows (e.g. 123-4567)')
       end
       it 'post_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
         @order_address.post_code = '1234567'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include('Post code is invalid. Include hyphen(-)')
+        expect(@order_address.errors.full_messages).to include('Postal code is invalid. Enter it as follows (e.g. 123-4567)')
       end
       it 'prefecture_idを選択していないと保存できないこと' do
         @order_address.prefecture_id = 1
@@ -63,32 +64,57 @@ RSpec.describe OrderAddress, type: :model do
       it 'blockが空だと保存できないこと' do
         @order_address.block = ''
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Block can't be blank")
+        expect(@order_address.errors.full_messages).to include("Addresses can't be blank")
       end
       it 'phone_numberが空だと保存できないこと' do
         @order_address.phone_number = ''
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+        expect(@order_address.errors.full_messages).to include('Phone number is too short')
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid. Input only number')
       end
       it 'phone_numberが9桁以下では保存できないこと' do
         @order_address.phone_number = '090123456'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+        expect(@order_address.errors.full_messages).to include('Phone number is too short (minimum is 10 characters)')
       end
       it 'phone_numberが12桁以上では保存できないこと' do
         @order_address.phone_number = '090123456789'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+        expect(@order_address.errors.full_messages).to include('Phone number is too long (maximum is 11 characters)')
       end
       it 'phone_numberに半角数字以外が含まれていると保存できないこと' do
         @order_address.phone_number = '090-1234-5678'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include('Phone number is invalid')
+        expect(@order_address.errors.full_messages).to include('Phone number is invalid. Input only number')
       end
       it 'tokenが空だと保存できないこと' do
         @order_address.token = nil
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
+
+      it '空で送信したときのエラー順が見本アプリと一致すること' do
+        @order_address.post_code = ''
+        @order_address.prefecture_id = 1
+        @order_address.city = ''
+        @order_address.block = ''
+        @order_address.phone_number = ''
+        @order_address.token = nil
+
+        @order_address.valid?
+
+        expect(@order_address.errors.full_messages).to eq([
+          "Token can't be blank",
+          "Postal code can't be blank",
+          'Postal code is invalid. Enter it as follows (e.g. 123-4567)',
+          "Prefecture can't be blank",
+          "City can't be blank",
+          "Addresses can't be blank",
+          "Phone number can't be blank",
+          'Phone number is too short',
+          'Phone number is invalid. Input only number'
+        ])
       end
 
       it '同じitem_idがすでに購入済みだとsaveできないこと' do
